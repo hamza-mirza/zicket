39a21e75d876d880c498dbfed1f4f50ed8bc77a7
Hamza Shahid <hammzzeeee@gmail.com>
2021-03-28 15:12:53 +0100

    popup complete

---
+++ src/javascript/Popup/Popup.js
@@ -0,0 +1,25 @@
+class Popup {
+  constructor() {
+    this.popupTrigger = document.querySelector('.js_popup-trigger')
+    this.popup = document.querySelector('.js_popup')
+    this.overlay = document.querySelector('.js_overlay')
+    this.popupClose = document.querySelector('.js_close-popup')
+    this.events()
+  }
+  events() {
+    this.popupTrigger.addEventListener('click', () => this.handleLaunchPopup())
+    this.popupClose.addEventListener('click', () => this.handleClosePopup())
+  }
+
+  handleLaunchPopup() {
+    this.popup.classList.add('popup--visible')
+    this.overlay.classList.add('overlay--visible')
+  }
+
+  handleClosePopup() {
+    this.popup.classList.remove('popup--visible')
+    this.overlay.classList.remove('overlay--visible')
+  }
+}
+
+export default Popup
--- src/javascript/main.js
+++ src/javascript/main.js
@@ -2,9 +2,11 @@ import 'normalize.css'
 import '../styles/common'
 
 import Menu from './Menu/Menu'
+import Popup from './Popup/Popup'
 
 const main = async () => {
   new Menu()
+  new Popup()
 }
 
 document.addEventListener('DOMContentLoaded', main)
